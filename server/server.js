fdab1ae4be78797691842f28b2dfdcd06b6c4192
Matteo_G <matteo.gonzi99@gmail.com>
2023-09-09 15:50:45 +0200

    build(Deploy): Socket.io error 404

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 443;
+const port = 4000;
 
 const cors = require('cors');
 const https = require('https').Server(app);

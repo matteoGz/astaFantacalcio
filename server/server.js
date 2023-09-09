3c2e26bd450506c3a6ef315760f2b55774586699
Matteo_G <matteo.gonzi99@gmail.com>
2023-09-09 14:32:21 +0200

    build(Deploy): Change for CORS and https server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 4000;
 
 const cors = require('cors');
-const http = require('http').Server(app);
+const https = require('https').Server(app);
 
 const playerList = require('./model/playerList.json');
 const userList = require('./model/nomiUtenti');
@@ -18,9 +18,9 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-const socketIO = require('socket.io')(http, {
+const socketIO = require('socket.io')(https, {
     cors: {
-        origin: "https://asta-fantacalcio2023.vercel.app/"
+        origin: '*'
     }
 });
 
@@ -85,6 +85,6 @@ socketIO.on('connection', (socket) => {
     });
 });
 
-http.listen(port, () => {
+https.listen(port, () => {
     console.log("Server started on "+port);
 })
